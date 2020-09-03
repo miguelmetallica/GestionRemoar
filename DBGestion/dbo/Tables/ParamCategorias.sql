5b73313c6425346189523360b2de2581cae67d32
Miguel Elias <miguel.a.elias@hotmail.com>
2020-09-03 10:28:32 -0300

    03-09-2020

--- DBGestion/dbo/Tables/ParamCategorias.sql
+++ DBGestion/dbo/Tables/ParamCategorias.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[ParamCategorias] (
     [Id]          NVARCHAR (150) NOT NULL,
+    [PadreId]     NVARCHAR (150) NULL,
     [Descripcion] NVARCHAR (150) NULL,
+    [Defecto]     BIT            NULL,
     [Estado]      BIT            NULL,
-    [FechaAlta]   DATETIME       NULL,
-    [UsuarioAlta] NVARCHAR (256) NULL,
-    [FechaEdit]   DATETIME       NULL,
-    [UsuarioEdit] NVARCHAR (256) NULL,
     CONSTRAINT [PK_ParamCategorias] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 
